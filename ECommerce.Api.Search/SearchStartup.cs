5ea70e00d46b383fc06dbfa57bc573ef280bc691
MrZahn <mykhailo.zhar@stud.onu.edu.ua>
2024-05-24 17:45:55 +0300

    Add docker-compose

--- ECommerce.Api.Search/SearchStartup.cs
+++ ECommerce.Api.Search/SearchStartup.cs
@@ -20,15 +20,15 @@ namespace ECommerce.Api.Search
             builder.Services.AddHttpClient("OrdersService", config =>
             {
                 config.BaseAddress = new Uri(builder.Configuration["Services:Orders"]);            
-            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(1, _ => TimeSpan.FromMilliseconds(500)));
             builder.Services.AddHttpClient("ProductsService", config =>
             {
                 config.BaseAddress = new Uri(builder.Configuration["Services:Products"]);
-            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(1, _ => TimeSpan.FromMilliseconds(500)));
             builder.Services.AddHttpClient("CustomersService", config =>
             {
                 config.BaseAddress = new Uri(builder.Configuration["Services:Customers"]);
-            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(1, _ => TimeSpan.FromMilliseconds(500)));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
